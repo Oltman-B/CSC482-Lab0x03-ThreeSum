49f3c57ba09af89a20807af38b663e58270f62fe
Baruch Oltman <42524526+Oltman-B@users.noreply.github.com>
2020-10-03 09:06:37 -0500

    Creates vs project

---
+++ Lab0x03/Lab0x03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab0x03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
